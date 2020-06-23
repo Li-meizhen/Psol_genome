1ba94c18158b2ecc7920f91e57549050d6e261a9
Li-meizhen <ycpllmz@gmail.com>
2020-06-23 11:08:04 +0800

    sort gff by chr

--- ConvertAnnotation/change_omiga_to_HiC_format.sh
+++ ConvertAnnotation/change_omiga_to_HiC_format.sh
@@ -24,7 +24,20 @@ perl splitBin2chromosome.pl $hicassembly ${omigagff}_splitBin_complete HiC_extra
 awk '{if($3=="exon") {print $9}}' HiC_extra_exon.temp |sed 's/.*\(PSOL[0-9]*\).*/\1/' |sort |uniq >all_exon.temp
 awk '{if($3=="gene") {print $9}}' HiC_extra_exon.temp |sed 's/.*\(PSOL[0-9]*\).*/\1/' |sort |uniq >all_gene.temp
 grep -F -v -f all_gene.temp all_exon.temp >extra_exon.temp
-grep -F -v -f extra_exon.temp HiC_extra_exon.temp >$hicgff
+grep -F -v -f extra_exon.temp HiC_extra_exon.temp >HiC_not_sorted.temp
 
+# ---------------------sort gff3 files by chr and loci----------------------#
+sed -i 's/\tmaker/,\tmaker/' HiC_not_sorted.temp 
+awk '{print $1}' HiC_not_sorted.temp |sort -u >sort_1_chr.temp 
+awk '{if($3=="gene") {print }}' HiC_not_sorted.temp |sort -nk4,5 >sort_2_loci_sorted_genes.temp
+for chr in `cat sort_1_chr.temp`; do grep $chr sort_2_loci_sorted_genes.temp >>sort_3_chr_sorted_genes.temp; done
+sed 's/.*Name=//' sort_3_chr_sorted_genes.temp >sort_4_genelist.temp
+for gene in `cat sort_4_genelist.temp`;do grep $gene HiC_not_sorted.temp >>$hicgff;done
+
+
+# ----------------------- delete temp files--------------------------------#
 mkdir original_files
-mv $omigagff* *.temp $splitbin* $hicassembly* original_files
+mv $omigagff $splitbin $hicassembly original_files
+
+mkdir tmp
+mv $omigagff* *.temp $splitbin* $hicassembly*
